f39f6ca20922204dd4341e8271c740c4f263c17b
Jonathan Croskell <15827290+joncros@users.noreply.github.com>
2019-05-12 12:00:34 -0600

    register model classes with admin site.

--- words/admin.py
+++ words/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
+from words.models import Language, Word, PartOfSpeech, WordSet
+
+# register model classes so they are visible on admin site (admin/)
+admin.site.register(Language)
+admin.site.register(Word)
+admin.site.register(PartOfSpeech)
+admin.site.register(WordSet)
+
 
 class WordSetAdmin(admin.ModelAdmin):
-    """Override ModelAdmin save_model to attach the user (if logged in) when adding a new WordSet"""
     def save_model(self, request, obj, form, change):
+        """Override to attach the user (if logged in) when adding a new WordSet"""
         if change is False:
             # only change if instance is being saved for the first time
             obj.creator = request.user
